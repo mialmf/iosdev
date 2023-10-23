9d98a97f653eeb1fac6629396ef7b40adfded91b
Mia Felipe <52087354+mialmf@users.noreply.github.com>
2023-10-23 13:55:40 -0700

    welcomeView first commit

---
+++ welcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  welcomeView.swift
+//  iosdev
+//
+//  Created by Mia Felipe on 10/23/23.
+//
+
+import SwiftUI
+
+struct welcomeView: View {
+    var body: some View {
+        
+        Text("Mixtify")
+    }
+}
+
+struct welcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        welcomeView()
+    }
+}
