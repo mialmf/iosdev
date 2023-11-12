be3051a04ba4cc4c631de0aa2611666f512862de
AnnaSoHock <81744048+AnnaSoHock@users.noreply.github.com>
2023-11-12 02:14:51 -0800

    Add files via upload

--- GeneratePlaylist.swift
+++ GeneratePlaylist.swift
@@ -7,36 +7,62 @@
 
 import SwiftUI
 
-struct GeneratePlaylist: View {
-        var selectedGenreArray: [String]
 
+
+struct GeneratePlaylist: View {
+    
+    
+    
+    var array: [String]
+    
+    
     var body: some View {
-        Text("Selected Genres: \(selectedGenreArray.joined(separator: ", "))")
-        VStack {
-            Button("Click here"){
-                print("We made a playlist for you!")
-            }
         
-            Text("We made a playlist for you!")
-                .padding()
-            Image("playlist")
-                .resizable()
-                .frame(width: 200.0, height: 200.0)
-                .cornerRadius(10)
-                .padding(.bottom, 10.0)
-            Spacer()
+            VStack {
+                List(array, id: \.self) { genre in
+                    Text(genre)
+                }
+                .listStyle(InsetGroupedListStyle())
+                
+                Spacer()
+            }
+            VStack {
+                Button("Click here"){
+                    print("We made a playlist for you!")
+                }
+                
+                Text("We made a playlist for you!")
+                
+                
+                
+                    .padding()
+                Image("playlist")
+                    .resizable()
+                    .frame(width: 200.0, height: 200.0)
+                    .cornerRadius(10)
+                    .padding(.bottom, 10.0)
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text("Selected Genres:")
+                        .font(.headline)
+                        .padding(.bottom, 5)
+                    
+                    ForEach(array, id: \.self) { genre in
+                        Text(genre)
+                            .padding(.bottom, 2)
+                    }
+                }
+                
+            }
+            .navigationBarBackButtonHidden(true)
+            .padding()
+            
             
         }
-        .navigationBarBackButtonHidden(true)
-        .padding()
-        
-       
     }
-}
 
-struct GeneratePlaylist_Previews: PreviewProvider {
-    static var previews: some View {
-        GeneratePlaylist(selectedGenreArray: ["Pop", "Rock", "HipHop"])
-    }
-}
 
+#Preview {
+    GeneratePlaylist(array: ["pop", "hiphop"])
+}
--- GridView.swift
+++ GridView.swift
@@ -1,211 +1,182 @@
-//
-//  GridView.swift
-//  Mixtify
-//
-//  Created by Mia Felipe on 11/8/23.
-//
-
 import SwiftUI
 
 struct GridView: View {
     let columnLayout = Array(repeating: GridItem(), count: 2)
-    
+
     @State private var selectedGenre = Color.gray
-    public var selectedGenreArray: [String] = []
-    public var entriesMax: Int = 3
+    @State private var selectedGenreArray: [String] = []
     public var entriesTot: Int = 0
-    
+
     let allColors: [Color] = [
         .pink,
         .red
     ]
-    
+
     let allColors2: [Color] = [
-        .mint,
-        .blue
-    ]
-    
-    let allColors3: [Color] = [
-        .green,
+        .yellow,
         .purple
     ]
-    
+
+    let allColors3: [Color] = [
+        .green,
+        .blue
+    ]
+
     let allColors4: [Color] = [
         .orange,
-        .indigo]
-    
+        .indigo
+    ]
+
     var body: some View {
-        NavigationView{
-            
+        NavigationView {
+
+            var entriesMax = 3
+            var entriesTotal = 0
+
             VStack {
-                Text("Pick 3 genres")
+                Text("Pick 3 colors!")
                     .font(.body)
                     .fontWeight(.bold)
                     .foregroundColor(selectedGenre)
-                    .padding(10)
-                
-                ScrollView {
-                    LazyVGrid(columns: columnLayout){
-                        ForEach(allColors, id: \.description) {
-                            color in Button {
-                                selectedGenre = color
-                                if (selectedGenre == Color.pink){
-                                    genreArray(selectedGenre: "pop")
-                                } else {
-                                    genreArray(selectedGenre: "rock")
-                                }
-                            } label: {
-                                RoundedRectangle(cornerRadius: 4.0)
-                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
-                                    .foregroundColor(color)
-                                
-                            }
-                            .buttonStyle(.plain)
-                        }
-                    }
-                    
-                    LazyVGrid(columns: columnLayout){
-                        ForEach(allColors2, id: \.description) {
-                            color in Button {
-                                selectedGenre = color
-                                if (selectedGenre == Color.mint){
-                                    genreArray(selectedGenre: "hiphop")
-                                } else{
-                                    genreArray(selectedGenre: "lofi")
-                                }
-                                
-                                
-                            } label: {
-                                RoundedRectangle(cornerRadius: 4.0)
-                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
-                                    .foregroundColor(color)
-                            }
-                            .buttonStyle(.plain)
-                        }
-                    }
-                    
-                    LazyVGrid(columns: columnLayout){
-                        ForEach(allColors3, id: \.description) {
-                            color in Button {
-                                selectedGenre = color
-                                if (selectedGenre == Color.green){
-                                    genreArray(selectedGenre: "oldies")
-                                } else {
-                                    genreArray(selectedGenre: "indie")
-                                }
-                            } label: {
-                                RoundedRectangle(cornerRadius: 4.0)
-                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
-                                    .foregroundColor(color)
-                            }
-                            .buttonStyle(.plain)
-                        }
-                    }
-                    
-                    
-                    LazyVGrid(columns: columnLayout){
-                        ForEach(allColors3, id: \.description) {
-                            color in Button {
-                                selectedGenre = color
-                                if (selectedGenre == Color.orange){
-                                    genreArray(selectedGenre: "edm")
-                                } else {
-                                    genreArray(selectedGenre: "kpop")
-                                }
-                            } label: {
-                                RoundedRectangle(cornerRadius: 4.0)
-                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
-                                    .foregroundColor(color)
-                            }
-                            .buttonStyle(.plain)
-                        }
-                    }
-                    
-                }
-                .padding(10)
-                NavigationLink("Generate Playlist!", destination: GeneratePlaylist(selectedGenreArray: selectedGenreArray))
-                    
+                    .padding(5)
 
-            }
-            
+                ScrollView {
+                    LazyVGrid(columns: columnLayout) {
+                        ForEach(allColors, id: \.description) { color in
+                            Button {
+                                selectedGenre = color
+                                if (selectedGenre == Color.pink) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("pop")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+
+                                if (selectedGenre == Color.red) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("rock")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+                            } label: {
+                                RoundedRectangle(cornerRadius: 4.0)
+                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                    .foregroundColor(color)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    } // end of button row 1
+
+                    LazyVGrid(columns: columnLayout) {
+                        ForEach(allColors2, id: \.description) { color in
+                            Button {
+                                selectedGenre = color
+                                if (selectedGenre == Color.yellow) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("hiphop")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+
+                                if (selectedGenre == Color.purple) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("lofi")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+                            } label: {
+                                RoundedRectangle(cornerRadius: 4.0)
+                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                    .foregroundColor(color)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    }   // end of button row 2
+
+                    LazyVGrid(columns: columnLayout) {
+                        ForEach(allColors3, id: \.description) { color in
+                            Button {
+                                selectedGenre = color
+                                if (selectedGenre == Color.green) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("oldies")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+                                if (selectedGenre == Color.blue) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("indie")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+                            } label: {
+                                RoundedRectangle(cornerRadius: 4.0)
+                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                    .foregroundColor(color)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    } // end of button row 3
+
+                    LazyVGrid(columns: columnLayout) {
+                        ForEach(allColors4, id: \.description) { color in
+                            Button {
+                                selectedGenre = color
+                                if (selectedGenre == Color.orange) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("edm")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+
+                                if (selectedGenre == Color.purple) {
+                                    if (entriesTotal < entriesMax) {
+                                        selectedGenreArray.append("kpop")
+                                        entriesTotal += 1
+                                        print("Selected Genres: \(selectedGenreArray)")
+                                    } else {
+                                        print("Max entries reached")
+                                    }
+                                }
+                            } label: {
+                                RoundedRectangle(cornerRadius: 4.0)
+                                    .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                    .foregroundColor(color)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    } // end of button row 4
+                } // end of scroll view
+                .padding(10)
+
+                NavigationLink("Generate Playlist!", destination: GeneratePlaylist(array: selectedGenreArray))
+                    .onAppear {
+                        print("Selected Genres in GridView: \(selectedGenreArray)")
+                    } // end of vstack
+
+            } // end of navi
             .padding(10)
-            
-            
-            
-        }
-    }
-    
-    
-    
-    func genreArray(selectedGenre: String){
-        var selectedGenreArray: [String] = []
-        let entriesMax: Int = 3
-        var entriesTot: Int = 0
-        
-        if (selectedGenre == "pop") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("pop")
-                entriesTot+=1
-            }
-        }
-        
-        if (selectedGenre == "rock") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("rock")
-                entriesTot+=1
-            }
-        }
-        
-        if (selectedGenre == "hiphop") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("hiphop")
-                entriesTot+=1
-            }
-        }
-        
-        if (selectedGenre == "lofi") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("lofi")
-                entriesTot+=1
-            }
-        }
-        
-        if (selectedGenre == "oldies") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("oldies")
-                entriesTot+=1
-            }
-        }
-        
-        if (selectedGenre == "indie") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("indie")
-                entriesTot+=1
-            }
-        }
-        
-        if (selectedGenre == "edm") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("edm")
-                entriesTot+=1
-            }
-            
-        }
-        
-        if (selectedGenre == "kpop") {
-            if (entriesTot < entriesMax){
-                selectedGenreArray.append("kpop")
-                entriesTot+=1
-            }
-        }
-        
-    } //end of function
-    
-    
-    struct GridView_Previews: PreviewProvider {
-        static var previews: some View {
-            GridView()
         }
     }
 }
-
