1b5834a21c22236f5a765678e2ba0d626d1a76d5
AnnaSoHock <81744048+AnnaSoHock@users.noreply.github.com>
2023-11-12 18:57:45 -0800

    Add files via upload

---
+++ Mixtify/Mixtify/Constants.swift
@@ -0,0 +1,39 @@
+//
+//  Constants.swift
+//  Mixtify
+//
+//  Created by Anna Sophia Hock on 11/12/23.
+//
+
+import Foundation
+
+//this information allows us to connect to the spotify app on backend side
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"Mixtify://")!
+let spotifyClientId = "ca1324de44da45e58acb403e9fda3f9d"
+let spotifyClientSecretKey = "cd383790cecf45e0b104e7e403988478"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
---
+++ Mixtify/Mixtify/MixtifyApp.swift
@@ -0,0 +1,18 @@
+//
+//  MixtifyApp.swift
+//  Mixtify
+//
+//  Created by Anna Sophia Hock on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MixtifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+                WelcomeView()
+            
+        }
+    }
+}
