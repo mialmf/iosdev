bd647941ee8461dee7d9fda9e3b0511e3bf95290
AnnaSoHock <81744048+AnnaSoHock@users.noreply.github.com>
2023-11-16 23:54:13 -0800

    Add files via upload
    Final MixtifyDemo (entire prorgram) commit

--- MixtifyDemo/MixtifyDemo/GeneratePlaylist.swift
+++ MixtifyDemo/MixtifyDemo/GeneratePlaylist.swift
@@ -1,68 +1,131 @@
-//
-//  ContentView.swift
-//  ios music app
-//
-//
-//
-
 import SwiftUI
 
-
-
 struct GeneratePlaylist: View {
-    
-    
-    
+
     var array: [String]
     
+    var genres: [String] = ["pop", "rock", "hiphop", "lofi", "oldies", "indie", "edm", "kpop"] // Add other genres as needed
     
-    var body: some View {
+    var songs: [String: [String]] = [
+        "pop": [
+            "Blinding Lights by The Weeknd",
+            "Watermelon Sugar by Harry Styles",
+            "Dynamite by BTS",
+            "Levitating by Dua Lipa",
+            "Say So by Doja Cat",
+            "Don't Start Now by Dua Lipa",
+            "Rain On Me by Lady Gaga and Ariana Grande",
+            "Intentions by Justin Bieber ft. Quavo",
+            "Adore You by Harry Styles"
+        ],
+        "rock": [
+            "Take Me to Church by Hozier",
+            "Believer by Imagine Dragons",
+            "Heathens by Twenty One Pilots",
+            "Uptown Funk by Mark Ronson ft. Bruno Mars"
+        ],
+        "hiphop": [
+            "Sicko Mode by Travis Scott",
+            "Old Town Road by Lil Nas X ft. Billy Ray Cyrus",
+            "Blinding Lights by The Weeknd (blending R&B and hip-hop elements)",
+            "WAP by Cardi B ft. Megan Thee Stallion",
+            "The Box by Roddy Ricch",
+            "Rockstar by DaBaby ft. Roddy Ricch"
+        ],
+        "lofi": [
+            "Jocelyn Flores by XXXTentacion (lofi remix)",
+            "Death Bed (Coffee for Your Head) by Powfu ft. beabadoobee (lofi remix)",
+            "lofi hip hop radio - beats to relax/study to by ChilledCow (YouTube live stream)",
+            "The Girl I Haven't Met by Kudasaibeats",
+            "Dreams by SwuM"
+        ],
+        "oldies": [
+            "Johnny B. Goode by Chuck Berry",
+            "I Want to Hold Your Hand by The Beatles",
+            "What'd I Say by Ray Charles",
+            "Twist and Shout by The Isley Brothers (covered by The Beatles)",
+            "Stand by Me by Ben E. King"
+        ],
+        "indie": [
+            "Take Me Out by Franz Ferdinand",
+            "Electric Feel by MGMT",
+            "Ho Hey by The Lumineers",
+            "Dog Days Are Over by Florence + The Machine",
+            "Pumped Up Kicks by Foster the People"
+        ],
+        "edm": [
+            "Strobe by Deadmau5",
+            "Wake Me Up by Avicii",
+            "Clarity by Zedd ft. Foxes",
+            "Titanium by David Guetta ft. Sia",
+            "Satisfaction by Benny Benassi"
+        ],
+        "kpop": [
+            "Lovesick Girls by BLACKPINK",
+            "Boy With Luv by BTS ft. Halsey",
+            "Kill This Love by BLACKPINK",
+            "Permission to Dance by BTS",
+            "On The Ground by ROSÉ"
+        ],
+    ]
+    @State private var showGenresAndSongs = false
         
-            VStack {
-                List(array, id: \.self) { genre in
-                    Text(genre)
-                }
-                .listStyle(InsetGroupedListStyle())
-                
-                Spacer()
-            }
-            VStack {
-                Button("Click here"){
-                    print("We made a playlist for you!")
-                }
-                
-                Text("We made a playlist for you!")
-                
-                
-                
-                    .padding()
-                Image("playlist") //provide asset for this
-                    .resizable()
-                    .frame(width: 200.0, height: 200.0)
-                    .cornerRadius(10)
-                    .padding(.bottom, 10.0)
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("Selected Genres:")
-                        .font(.headline)
-                        .padding(.bottom, 5)
+        var body: some View {
+            ScrollView {
+                VStack {
+                   
                     
-                    ForEach(array, id: \.self) { genre in
-                        Text(genre)
-                            .padding(.bottom, 2)
+                    Text("We made a playlist for you!")
+                        .padding()
+                    
+                    Image("playlist") // provide asset for this
+                        .resizable()
+                        .frame(width: 200.0, height: 200.0)
+                        .cornerRadius(10)
+                        .padding(.bottom, 10.0)
+                    
+                    Button("Click here") {
+                        showGenresAndSongs.toggle()
+                    }
+                    .padding()
+                    
+                    Spacer()
+                    
+                    if showGenresAndSongs {
+                        VStack(alignment: .leading) {
+                            Text("Selected Genres:")
+                                .font(.headline)
+                                .underline(true)
+                                .padding(.bottom, 5)
+                            
+                            ForEach(array, id: \.self) { genre in
+                                Text(genre)
+                                    .padding(.bottom, 2)
+                                    .bold()
+                                
+                                if let genreSongs = songs[genre] {
+                                    ForEach(genreSongs, id: \.self) { song in
+                                        Text(song)
+                                            .padding(.leading, 20) // Indent songs under genres
+                                            .padding(.bottom, 2)
+                                    }
+                                }
+                            }
+                        }
+                        
                     }
                 }
-                
+                .onChange(of: showGenresAndSongs) { _ in
+                    // If needed, you can perform additional actions when the state changes
+                }
             }
             .navigationBarBackButtonHidden(true)
             .padding()
-            
-            
         }
     }
 
-
-#Preview {
-    GeneratePlaylist(array: ["pop", "hiphop"])
-}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            GeneratePlaylist(array: ["pop", "hiphop"])
+        }
+    }
--- MixtifyDemo/MixtifyDemo/GridView.swift
+++ MixtifyDemo/MixtifyDemo/GridView.swift
@@ -2,47 +2,44 @@ import SwiftUI
 
 struct GridView: View {
     let columnLayout = Array(repeating: GridItem(), count: 2)
-    @State private var isShowingLoadingScreen = false
-    @State private var isShowingGeneratePlaylist = false
 
-    
     @State private var selectedGenre = Color.gray
     @State private var selectedGenreArray: [String] = []
     public var entriesTot: Int = 0
-    
+
     let allColors: [Color] = [
         .pink,
         .red
     ]
-    
+
     let allColors2: [Color] = [
         .yellow,
         .purple
     ]
-    
+
     let allColors3: [Color] = [
         .green,
         .blue
     ]
-    
+
     let allColors4: [Color] = [
         .orange,
         .indigo
     ]
-    
+
     var body: some View {
         NavigationView {
-            
+
             var entriesMax = 3
             var entriesTotal = 0
-            
+
             VStack {
                 Text("Pick 3 colors!")
                     .font(.body)
                     .fontWeight(.bold)
                     .foregroundColor(selectedGenre)
                     .padding(5)
-                
+
                 ScrollView {
                     LazyVGrid(columns: columnLayout) {
                         ForEach(allColors, id: \.description) { color in
@@ -53,16 +50,35 @@ struct GridView: View {
                                         selectedGenreArray.append("pop")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Blinding Lights" by The Weeknd
+                                            "Watermelon Sugar" by Harry Styles
+                                            "Dynamite" by BTS
+                                            "Levitating" by Dua Lipa
+                                            "Say So" by Doja Cat
+                                            "Don't Start Now" by Dua Lipa
+                                            "Rain On Me" by Lady Gaga and Ariana Grande
+                                            "Intentions" by Justin Bieber ft. Quavo
+                                            "Adore You" by Harry Styles
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
                                 }
-                                
+
                                 if (selectedGenre == Color.red) {
                                     if (entriesTotal < entriesMax) {
                                         selectedGenreArray.append("rock")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Take Me to Church" by Hozier
+                                            "Believer" by Imagine Dragons
+                                            "Heathens" by Twenty One Pilots
+                                            "Uptown Funk" by Mark Ronson ft. Bruno Mars (blending funk and pop-rock elements)
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
@@ -75,7 +91,7 @@ struct GridView: View {
                             .buttonStyle(.plain)
                         }
                     } // end of button row 1
-                    
+
                     LazyVGrid(columns: columnLayout) {
                         ForEach(allColors2, id: \.description) { color in
                             Button {
@@ -85,16 +101,33 @@ struct GridView: View {
                                         selectedGenreArray.append("hiphop")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Sicko Mode" by Travis Scott
+                                            "Old Town Road" by Lil Nas X ft. Billy Ray Cyrus
+                                            "Blinding Lights" by The Weeknd (blending R&B and hip-hop elements)
+                                            "WAP" by Cardi B ft. Megan Thee Stallion
+                                            "The Box" by Roddy Ricch
+                                            "Rockstar" by DaBaby ft. Roddy Ricch
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
                                 }
-                                
+
                                 if (selectedGenre == Color.purple) {
                                     if (entriesTotal < entriesMax) {
                                         selectedGenreArray.append("lofi")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Jocelyn Flores" by XXXTentacion (lofi remix)
+                                            "Death Bed (Coffee for Your Head)" by Powfu ft. beabadoobee (lofi remix)
+                                            "lofi hip hop radio - beats to relax/study to" by ChilledCow (YouTube live stream)
+                                            "The Girl I Haven't Met" by Kudasaibeats
+                                            "Dreams" by SwuM
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
@@ -107,7 +140,7 @@ struct GridView: View {
                             .buttonStyle(.plain)
                         }
                     }   // end of button row 2
-                    
+
                     LazyVGrid(columns: columnLayout) {
                         ForEach(allColors3, id: \.description) { color in
                             Button {
@@ -117,6 +150,14 @@ struct GridView: View {
                                         selectedGenreArray.append("oldies")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Johnny B. Goode" by Chuck Berry
+                                            "I Want to Hold Your Hand" by The Beatles
+                                            "What'd I Say" by Ray Charles
+                                            "Twist and Shout" by The Isley Brothers (covered by The Beatles)
+                                            "Stand by Me" by Ben E. King
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
@@ -126,6 +167,14 @@ struct GridView: View {
                                         selectedGenreArray.append("indie")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Take Me Out" by Franz Ferdinand
+                                            "Electric Feel" by MGMT
+                                            "Ho Hey" by The Lumineers
+                                            "Dog Days Are Over" by Florence + The Machine
+                                            "Pumped Up Kicks" by Foster the People
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
@@ -138,7 +187,7 @@ struct GridView: View {
                             .buttonStyle(.plain)
                         }
                     } // end of button row 3
-                    
+
                     LazyVGrid(columns: columnLayout) {
                         ForEach(allColors4, id: \.description) { color in
                             Button {
@@ -148,16 +197,32 @@ struct GridView: View {
                                         selectedGenreArray.append("edm")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Strobe" by Deadmau5
+                                            "Wake Me Up" by Avicii
+                                            "Clarity" by Zedd ft. Foxes
+                                            "Titanium" by David Guetta ft. Sia
+                                            "Satisfaction" by Benny Benassi
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
                                 }
-                                
+
                                 if (selectedGenre == Color.purple) {
                                     if (entriesTotal < entriesMax) {
                                         selectedGenreArray.append("kpop")
                                         entriesTotal += 1
                                         print("Selected Genres: \(selectedGenreArray)")
+                                        print("""
+                                            "Lovesick Girls" by BLACKPINK
+                                            "Boy With Luv" by BTS ft. Halsey
+                                            "Kill This Love" by BLACKPINK
+                                            "Permission to Dance" by BTS
+                                            "On The Ground" by ROSÉ
+                                            """)
+
                                     } else {
                                         print("Max entries reached")
                                     }
@@ -172,38 +237,14 @@ struct GridView: View {
                     } // end of button row 4
                 } // end of scroll view
                 .padding(10)
-                
-                // destination: GeneratePlaylist(array: selectedGenreArray)
-                Button("Generate Playlist!") {
-                                   isShowingLoadingScreen = true
-                                   // Simulate loading for 5 seconds
-                                   DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                                       isShowingLoadingScreen = false
-                                       isShowingGeneratePlaylist = true
-                                      
-                                      
-                                   }
-                               }
-                               .padding(10)
 
-                               NavigationLink(
-                                   destination: GeneratePlaylist(array: selectedGenreArray),
-                                   isActive: $isShowingGeneratePlaylist
-                               ) {
-                                   EmptyView()
-                               }
-                               .hidden()
-                           }
-                           .background(
-                               Color.white
-                           )
-                           .padding(10)
-                           .sheet(isPresented: $isShowingLoadingScreen) {
-                               LoadingScreenView()
-                           }
+                NavigationLink("Generate Playlist!", destination: GeneratePlaylist(array: selectedGenreArray))
+                    .onAppear {
+                        print("Selected Genres in GridView: \(selectedGenreArray)")
+                    } // end of vstack
+
+            } // end of navi
+            .padding(10)
         }
     }
-    
-    
-    
 }
